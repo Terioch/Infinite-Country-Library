f4ea58513a5e178f696665a55380230bc93a18ff
Rio <riostockton@gmail.com>
2022-02-21 20:49:42 +0000

    Re-configured api request functionality and information display styling

--- app.js
+++ app.js
@@ -1,137 +1,268 @@
 class Country {
-  // instantiate a class blueprint
-  constructor() {
-    this.dropdown = document.getElementById('dropdown')
-    this.countryList = document.querySelector('.country-list')
-    this.countryTable = document.querySelector('.country-table')
-    this.tableCells = document.getElementsByTagName('td')
-    this.populateDropdown()
-    this.events()
-  }
+	// instantiate a class blueprint
+	constructor() {
+		this.dropdown = document.getElementById("dropdown");
+		this.countryList = document.querySelector(".country-list");
+		this.countryTable = document.querySelector(".info-container");
+		this.tableCells = document.getElementsByTagName("td");
+		this.populateDropdown();
+		this.events();
+	}
 
-  // events
-  events() {
-    this.dropdown.onchange = () => {
-      if (this.dropdown.value.length == 1) this.getCountryNames()
-    }
-  }
+	// events
+	events() {
+		this.dropdown.onchange = () => {
+			if (this.dropdown.value.length == 1) this.getCountryNames();
+		};
+	}
 
-  // methods will go here
+	// populates select dropdown with letters from A-Z
+	populateDropdown() {
+		let ascii = 65;
 
-  // populates select dropdown with letters from A-Z
-  populateDropdown() {
-    let ascii = 65
+		while (ascii <= 90) {
+			let element = document.createElement("option"); // create new option
 
-    while (ascii <= 90) {
-      let element = document.createElement('option') // create new option
+			// insert letter into new option element and append onto dropdown
+			element.textContent = String.fromCharCode(ascii);
+			this.dropdown.appendChild(element);
+			ascii++;
+		}
+	}
 
-      // insert letter into new option element and append onto dropdown
-      element.textContent = String.fromCharCode(ascii)
-      this.dropdown.appendChild(element)
-      ascii++
-    }
-  }
+	// fetch data from restcountries API
+	async getCountryNames() {
+		try {
+			const response = await fetch(
+				"http://api.worldbank.org/v2/country?per_page=299&format=json"
+			);
+			// const response = await fetch(
+			// 	`http://api.countrylayer.com/v2/all?access_key=627b90083cdb18bfd7935645a501eb36`
+			// );
+			const data = await response.json();
+			console.log(data);
 
-  // fetch data from restcountries API
-  async getCountryNames() {
-    try {
-      const response = await fetch("https://restcountries.eu/rest/v2/all")
-      const data = await response.json()
-      // iterate over data and retrieve all country names
-      const countries = data.map((country) => {
-        return country.name
-      })
-      this.loadByLetter(countries) 
-    } catch(e) {
-      console.log("Failed to fetch all countries.", e)
-    }
-  }
+			// iterate over data and retrieve all country names
+			const countries = data[1].map((country) => ({
+				name: country.name,
+				isoCode: country.iso2Code,
+			}));
+			this.loadByLetter(countries);
+		} catch (e) {
+			console.error("Failed to fetch all countries.", e);
+		}
+	}
 
-  // load and output all countries beginning with corresponding letter
-  loadByLetter(countries) {
-    this.countryList.innerHTML = '' // empty country list
+	// load and output all countries beginning with corresponding letter
+	loadByLetter(countries) {
+		this.countryList.innerHTML = ""; // empty country list
 
-    for (let i = 0; i < countries.length; ++i) {
-      // insert country with corresponding starting letter into list
-      if (countries[i][0] == this.dropdown.value) {
-        let countryListItem = document.createElement('li')
-        this.countryList.appendChild(countryListItem)
-        countryListItem.innerHTML = countries[i]
-        countryListItem.onclick = () => this.getCountryData(countryListItem)
-      } 
-    }
-  }
+		for (let country of countries) {
+			// insert country with corresponding starting letter into list
+			if (country.name[0] == this.dropdown.value) {
+				let countryListItem = document.createElement("li");
+				countryListItem.innerHTML = country.name;
+				countryListItem.setAttribute("name", country.isoCode);
+				this.countryList.appendChild(countryListItem);
+				countryListItem.onclick = () => this.getCountryData(country);
+			}
+		}
+	}
 
-  // fetch data from specific country 
-  async getCountryData(countryListItem) {
-    try {
-      const response = await fetch(`https://restcountries.eu/rest/v2/name/${countryListItem.textContent}?fullText=true`)
-      const data = await response.json()
-      this.displayDataInTable(data)
-      this.countryTable.style.display = 'table' // display country table
-    } catch(e) {
-      console.error("Failed to fetch country.", e)
-    }
-  }
+	// fetch data from specific country
+	async getCountryData(country) {
+		try {
+			// const response = await fetch(
+			// 	`http://api.countrylayer.com/v2/name/${country}?access_key=627b90083cdb18bfd7935645a501eb36?fullText=true`
+			// );
+			const response = await fetch(
+				`http://api.worldbank.org/v2/country/${country.isoCode}?format=json`
+			);
+			const data = await response.json();
+			console.log(data[1][0].capitalCity);
+			this.displayDataInTable(data[1][0]);
+			// this.countryTable.style.display = "table"; // display country table
+		} catch (e) {
+			console.error("Failed to fetch country.", e);
+		}
+	}
 
-  // display data for corresponding country within a table
-  displayDataInTable(data) {
-    // extract desired properties from country data
-    const tableData = this.extractData(data, ["name", "capital", "area", "region", "subregion", "population", "borders", "timezones", 
-      "languages", "currencies", "demonym", "latlng", "gini", "nativeName", "numericCode", "translations", "flag"]) // array of desired keys
+	// display data for corresponding country within a table
+	displayDataInTable(country) {
+		console.log(country);
+		document.querySelector(".info-container").innerHTML = `
+      <ul class="d-flex flex-wrap">
+        <li class="list-group-item list-group-item-light rounded-0 border-start-0 border-top-0 border-bottom-0">
+          <p class="fw-bold mb-1">Name</p>
+          <p>${country.name}</p>  
+        </li>
+        <li class="list-group-item list-group-item-light rounded-0 border-start-0 border-top-0 border-bottom-0">
+          <p class="fw-bold mb-1">Iso Code</p>
+          <p>${country.iso2Code}</p>  
+        </li>
+        <li class="list-group-item list-group-item-light rounded-0 border-start-0 border-top-0 border-bottom-0">
+          <p class="fw-bold mb-1">Capital City</p>
+          <p>${country.capitalCity || "none"}</p>      
+        </li>
+        <li class="list-group-item list-group-item-light rounded-0 border-start-0 border-top-0 border-bottom-0">
+          <p class="fw-bold mb-1">Region</p>
+          <p>${country.region.value || "none"}</p>      
+        </li>
+        <li class="list-group-item list-group-item-light rounded-0 border-start-0 border-top-0 border-bottom-0">
+          <p class="fw-bold mb-1">Population</p>
+          <p>Available Soon</p>      
+        </li>
+        <li class="list-group-item list-group-item-light rounded-0 border-start-0 border-top-0 border-bottom-0">
+          <p class="fw-bold mb-1">Income Level</p>
+          <p>${country.incomeLevel.value || "none"}</p>      
+        </li>
+        <li class="list-group-item list-group-item-light rounded-0 border-start-0 border-top-0 border-bottom-0">
+          <p class="fw-bold mb-1">Latitude</p>
+          <p>${country.latitude || "none"}</p>      
+        </li>        
+        <li class="list-group-item list-group-item-light rounded-0 border-start-0 border-top-0 border-bottom-0">
+          <p class="fw-bold mb-1">Longitude</p>
+          <p>${country.longitude || "none"}</p>      
+        </li>        
+        <li class="list-group-item list-group-item-light rounded-0 border-start-0 border-top-0 border-bottom-0">
+          <p class="fw-bold mb-1">Currencies</p>
+          <p>Available Soon</p>      
+        </li>
+        <li class="list-group-item list-group-item-light rounded-0 border-start-0 border-top-0 border-bottom-0">
+          <p class="fw-bold mb-1">Langauges</p>
+          <p>Available Soon</p>      
+        </li>
+      </ul>
+    `;
+		// document.querySelector(".info-container").innerHTML = `
+		//   <table class="country-table">
+		//     <tr>
+		//       <th>Name</th>
+		//       <td>${country.name}</td>
+		//     </tr>
+		//     <tr>
+		//       <th>Name</th>
+		//       <td>${country.iso2Code}</td>
+		//     </tr>
+		//     <tr>
+		//       <th>Capital</th>
+		//       <td>${country.capitalCity}</td>
+		//     </tr>
+		//     <tr>
+		//       <th>Region</th>
+		//       <td>${country.region.value}</td>
+		//     </tr>
+		//     <tr>
+		//       <th>Population</th>
+		//       <td>Available soon</td>
+		//     </tr>
+		//     <tr>
+		//       <th>Income Level</th>
+		//       <td>${country.incomeLevel.value}</td>
+		//     </tr>
+		//     <tr>
+		//       <th>Latitude/longitude</th>
+		//       <td>${country.latitude} / ${country.longitude}</td>
+		//     </tr>
+		//     <tr>
+		//       <th>Timezones</th>
+		//       <td>Available Soon</td>
+		//     </tr>
+		//     <tr>
+		//       <th>Currencies</th>
+		//       <td>Available Soon</td>
+		//     </tr>
+		//     <tr>
+		//       <th>Languages</th>
+		//       <td>Available Soon</td>
+		//     </tr>
+		//   </table>
+		// `;
+	}
+	// displayDataInTable(data) {
+	// 	// extract desired properties from country data
+	// 	const tableData = this.extractData(data, [
+	// 		"name",
+	// 		"capital",
+	// 		"area",
+	// 		"region",
+	// 		"subregion",
+	// 		"population",
+	// 		"borders",
+	// 		"timezones",
+	// 		"languages",
+	// 		"currencies",
+	// 		"demonym",
+	// 		"latlng",
+	// 		"gini",
+	// 		"nativeName",
+	// 		"numericCode",
+	// 		"translations",
+	// 		"flag",
+	// 	]); // array of desired keys
 
-    let tableDataArr = Object.keys(tableData) // initialise new object as array
-   
-    // iterate over table data array
-    for (let i = 0; i < tableDataArr.length; ++i) {
-      // insert value of current object property into appropriate table cell
-      if (tableData[tableDataArr[i]] === null || tableData[tableDataArr[i]].length < 1) {
-        this.tableCells[i].textContent = "No " + tableDataArr[i]
-      } else {
-        if (tableDataArr[i] === "flag") {
-          this.renderImage(tableData.flag)
-        }
-        else if (tableDataArr[i] === 'currencies' || tableDataArr[i] === 'languages' || tableDataArr[i] === 'translations') {
-          // output object values containing objects in JSON string format
-          if (tableDataArr[i] === 'languages') {
-            let temp = []
-            // iterate over object within object value array and output name
-            for (let item of tableData[tableDataArr[i]]) {
-              temp.push(item.name) // push name value onto array
-            }
-            this.tableCells[i].textContent = temp.join(', ')
-          } else {
-            this.tableCells[i].textContent = JSON.stringify(tableData[tableDataArr[i]]).split(', ')
-          }
-        } else {
-          // insert object value into table cell with separation for arrays
-          Array.isArray(tableData[tableDataArr[i]]) ? this.tableCells[i].textContent = tableData[tableDataArr[i]].join(', ')
-            : this.tableCells[i].textContent = tableData[tableDataArr[i]]
-        }
-      }           
-    }
-  }
+	// 	let tableDataArr = Object.keys(tableData); // initialise new object as array
 
-  // extracts and stores neccessary data from GET request within new object
-  extractData(data, keys) {
-    const newData = {}
-    // store key/property within new object if included in keys array
-    Object.keys(data[0]).forEach((key) => {
-      if (keys.includes(key)) {
-        newData[key] = data[0][key]
-      }
-    })
-    return newData
-  }
+	// 	// iterate over table data array
+	// 	for (let i = 0; i < tableDataArr.length; ++i) {
+	// 		// insert value of current object property into appropriate table cell
+	// 		if (
+	// 			tableData[tableDataArr[i]] === null ||
+	// 			tableData[tableDataArr[i]].length < 1
+	// 		) {
+	// 			this.tableCells[i].textContent = "No " + tableDataArr[i];
+	// 		} else {
+	// 			if (tableDataArr[i] === "flag") {
+	// 				this.renderFlag(tableData.flag);
+	// 			} else if (
+	// 				tableDataArr[i] === "currencies" ||
+	// 				tableDataArr[i] === "languages" ||
+	// 				tableDataArr[i] === "translations"
+	// 			) {
+	// 				// output object values containing objects in JSON string format
+	// 				if (tableDataArr[i] === "languages") {
+	// 					let temp = [];
+	// 					// iterate over object within object value array and output name
+	// 					for (let item of tableData[tableDataArr[i]]) {
+	// 						temp.push(item.name); // push name value onto array
+	// 					}
+	// 					this.tableCells[i].textContent = temp.join(", ");
+	// 				} else {
+	// 					this.tableCells[i].textContent = JSON.stringify(
+	// 						tableData[tableDataArr[i]]
+	// 					).split(", ");
+	// 				}
+	// 			} else {
+	// 				// insert object value into table cell with separation for arrays
+	// 				Array.isArray(tableData[tableDataArr[i]])
+	// 					? (this.tableCells[i].textContent =
+	// 							tableData[tableDataArr[i]].join(", "))
+	// 					: (this.tableCells[i].textContent = tableData[tableDataArr[i]]);
+	// 			}
+	// 		}
+	// 	}
+	// }
 
-  // renders image with specified dimension and size values
-  renderImage(flag) {
-    const flagContainer = document.querySelector('.flag-container')
-    flagContainer.innerHTML = `
-      <img class="flag" src="${flag}" alt="Flag cannot be rendered">`
-    flagContainer.style.display = 'block' // display container for flag
-  }
+	// extracts and stores neccessary data from GET request within new object
+	extractData(data, keys) {
+		const newData = {};
+		// store key/property within new object if included in keys array
+		Object.keys(data[0]).forEach((key) => {
+			if (keys.includes(key)) {
+				newData[key] = data[0][key];
+			}
+		});
+		return newData;
+	}
+
+	// renders image with specified dimension and size values
+	renderFlag(flag) {
+		const flagContainer = document.querySelector(".flag-container");
+		flagContainer.innerHTML = `
+      <img class="flag" src="${flag}" alt="Flag cannot be rendered">`;
+		flagContainer.style.display = "block"; // display container for flag
+	}
 }
 
-// instantiate our class within an object
-const country = new Country()
+// instantiate our class as an object
+const country = new Country();
