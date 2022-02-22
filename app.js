8de4797ba1d14c10d618480b521ddb3c9a0d5ee1
Rio <riostockton@gmail.com>
2022-02-22 18:52:19 +0000

    Re-configured country data fetching for multiple APIs

--- app.js
+++ app.js
@@ -39,12 +39,11 @@ class Country {
 			// 	`http://api.countrylayer.com/v2/all?access_key=627b90083cdb18bfd7935645a501eb36`
 			// );
 			const data = await response.json();
-			console.log(data);
 
-			// iterate over data and retrieve all country names
+			// retrieve all country names and iso2 codes
 			const countries = data[1].map((country) => ({
 				name: country.name,
-				isoCode: country.iso2Code,
+				iso2Code: country.iso2Code,
 			}));
 			this.loadByLetter(countries);
 		} catch (e) {
@@ -68,121 +67,128 @@ class Country {
 		}
 	}
 
-	// fetch data from specific country
-	async getCountryData(country) {
+	// create the country data view model
+	createCountryViewModel(wbData, rcData) {
+		return {
+			name: wbData.name || "Not Applicable",
+			iso2Code: wbData.iso2Code || "Not Applicable",
+			capitalCity: wbData.capitalCity || "Not Applicable",
+			region: wbData.region || "Not Applicable",
+			size: rcData.size || "Not Applicable",
+			population: rcData.population || "Not Applicable",
+			currency: rcData.currency || "Not Applicable",
+			phoneCode: rcData.phone_code || "Not Applicable",
+			incomeLevel: wbData.incomeLevel || "Not Applicable",
+			lendingType: wbData.lendingType || "Not Applicable",
+			latitude: wbData.latitude || "Not Applicable",
+			longitude: wbData.longitude || "Not Applicable",
+			flag: rcData.href?.flag || "./images/fake-flag.jpg",
+		};
+	}
+
+	async getRestfulCountriesData(country) {
 		try {
-			const x = await fetch(
-				`https://restfulcountries.com/api/v1/countries/Nigeria`,
+			const response = await fetch(
+				`https://restfulcountries.com/api/v1/countries/${country.name}`,
 				{
 					method: "GET",
 					headers: {
 						"Content-Type": "application/json",
+						Authorization: "Bearer 55|tGYLGHgoBRLfheDDikCsyrKarReWHzIUuP3NTKid",
 					},
 				}
 			);
-			console.log(await x.json());
+			const data = await response.json();
+			return data.data || {};
+		} catch (e) {
+			console.error("Failed to fetch restfulcountries data: ", e);
+		}
+	}
+
+	async getWorldBankData(country) {
+		try {
 			const response = await fetch(
-				`http://api.worldbank.org/v2/country/${country.isoCode}?format=json`
+				`http://api.worldbank.org/v2/country/${country.iso2Code}?format=json`
 			);
 			const data = await response.json();
-			this.displayDataInTable(data[1][0]);
+			return data[1][0] || {};
 		} catch (e) {
-			console.error("Failed to fetch country.", e);
+			console.error("Failed to fetch worldbank data: ", e);
 		}
+	}
+
+	// fetch data for an individual country
+	async getCountryData(initialCountry) {
+		const worldBankData = await this.getWorldBankData(initialCountry);
+		const restfulCountriesData = await this.getRestfulCountriesData(
+			initialCountry
+		);
+
+		// Merge and filter data sets into a single object
+		const country = this.createCountryViewModel(
+			worldBankData,
+			restfulCountriesData
+		);
+		console.log({ country });
+		this.renderFlag(country.flag);
+		this.displayDataInTable(country);
 	}
 
 	// display data for corresponding country within a table
 	displayDataInTable(country) {
-		console.log(country);
 		document.querySelector(".info-container").innerHTML = `
-		  <div class="country-info-grid row">        
-        <div class="country-info-item-light col-12 col-md-4 col-sm-6 d-flex flex-column justify-content-center p-3 border">
-          <h2 class="country-info-title fs-5 fw-bold">Country Name</h2>
-          <p class="country-info-text" class="country-info-text">${
-						country.name
-					}</p>
+		  <div class="row">        
+        <div class="info-item-light col-12 col-md-4 col-sm-6 d-flex flex-column justify-content-center p-3 border">
+          <h2 class="info-title fs-5 fw-bold">Country Name</h2>
+          <p class="info-text">${country.name}</p>
         </div>       
-        <div class="country-info-item-dark col-12 col-md-4 col-sm-6 d-flex flex-column justify-content-center p-3 border">
-          <h2 class="country-info-title fs-5 fw-bold">ISO alpha-2 code</h2>
-          <p class="country-info-text">${country.iso2Code}</p>
+        <div class="info-item-dark col-12 col-md-4 col-sm-6 d-flex flex-column justify-content-center p-3 border">
+          <h2 class="info-title fs-5 fw-bold">ISO alpha-2 code</h2>
+          <p class="info-text">${country.iso2Code}</p>
         </div>      
-        <div class="country-info-item-light col-12 col-md-4 col-sm-6 d-flex flex-column justify-content-center p-3 border">
-          <h2 class="country-info-title fs-5 fw-bold">Capital</h2>
-          <p class="country-info-text">${country.capitalCity || "none"}</p>
+        <div class="info-item-light col-12 col-md-4 col-sm-6 d-flex flex-column justify-content-center p-3 border">
+          <h2 class="info-title fs-5 fw-bold">Capital</h2>
+          <p class="info-text">${country.capitalCity}</p>
         </div>       
-        <div class="country-info-item-dark col-12 col-md-4 col-sm-6 p-3 border">
-          <h2 class="country-info-title fs-5 fw-bold">Region</h2>
-          <p class="country-info-text">${country.region.value || "none"}</p>
+        <div class="info-item-dark col-12 col-md-4 col-sm-6 p-3 border">
+          <h2 class="info-title fs-5 fw-bold">Region</h2>
+          <p class="info-text">${country.region?.value}</p>
+        </div>
+				<div class="info-item-light col-12 col-md-4 col-sm-6 p-3 border">
+          <h2 class="info-title fs-5 fw-bold">Size</h2>
+          <p class="info-text">${country.size}</p>
+        </div>
+				<div class="info-item-dark col-12 col-md-4 col-sm-6 p-3 border">
+          <h2 class="info-title fs-5 fw-bold">Population</h2>
+          <p class="info-text">${country.population}</p>
+        </div>
+				<div class="info-item-light col-12 col-md-4 col-sm-6 p-3 border">
+          <h2 class="info-title fs-5 fw-bold">Currency</h2>
+          <p class="info-text">${country.currency}</p>
+        </div>
+				<div class="info-item-dark col-12 col-md-4 col-sm-6 p-3 border">
+          <h2 class="info-title fs-5 fw-bold">Phone Code</h2>
+          <p class="info-text">${country.phoneCode}</p>
         </div>       
-        <div class="country-info-item-light col-12 col-md-4 col-sm-6 p-3 border">
-          <h2 class="country-info-title fs-5 fw-bold">Lending Type</h2>
-          <p class="country-info-text">${country.lendingType.value}</p>
+				<div class="info-item-light col-12 col-md-4 col-sm-6 p-3 border">
+          <h2 class="info-title fs-5 fw-bold">Income Level</h2>
+          <p class="info-text">${country.incomeLevel?.value}</p>
+        </div> 
+        <div class="info-item-dark col-12 col-md-4 col-sm-6 p-3 border">
+          <h2 class="info-title fs-5 fw-bold">Lending Type</h2>
+          <p class="info-text">${country.lendingType?.value}</p>
+        </div>                   
+        <div class="info-item-light col-12 col-md-4 col-sm-6 p-3 border">
+          <h2 class="info-title fs-5 fw-bold">Latitude</h2>
+          <p class="info-text">${country.latitude}</p>
         </div>       
-        <div class="country-info-item-dark col-12 col-md-4 col-sm-6 p-3 border">
-          <h2 class="country-info-title fs-5 fw-bold">Income Level</h2>
-          <p class="country-info-text">${
-						country.incomeLevel.value || "none"
-					}</p>
-        </div>     
-        <div class="country-info-item-light col-12 col-md-4 col-sm-6 p-3 border">
-          <h2 class="country-info-title fs-5 fw-bold">Latitude</h2>
-          <p class="country-info-text">${country.latitude || "none"}</p>
-        </div>       
-        <div class="country-info-item-dark col-12 col-md-4 col-sm-6 p-3 border">
-          <h2 class="country-info-title fs-5 fw-bold">Longitude</h2>
-          <p class="country-info-text">${country.longitude || "none"}</p>
+        <div class="info-item-dark col-12 col-md-4 col-sm-6 p-3 border">
+          <h2 class="info-title fs-5 fw-bold">Longitude</h2>
+          <p class="info-text">${country.longitude}</p>
         </div>                          
       </div>
 		`;
 		this.countryList.innerHTML = "";
-		// document.querySelector(".info-container").innerHTML = `
-		//   <table class="table country-table" style="display: table">
-		//     <tr>
-		//       <th >Name</th>
-		//       <td>${country.name}</td>
-		//     </tr>
-		//     <tr>
-		//       <th>ISO alpha-2 code</th>
-		//       <td>${country.iso2Code}</td>
-		//     </tr>
-		//     <tr>
-		//       <th>Capital</th>
-		//       <td>${country.capitalCity}</td>
-		//     </tr>
-		//     <tr>
-		//       <th>Region</th>
-		//       <td>${country.region.value || "none"}</td>
-		//     </tr>
-		//     <tr>
-		//       <th>Population</th>
-		//       <td>Available soon</td>
-		//     </tr>
-		//     <tr>
-		//       <th>Income Level</th>
-		//       <td>${country.incomeLevel.value || "none"}</td>
-		//     </tr>
-		//     <tr>
-		//       <th>Latitude</th>
-		//       <td>${country.latitude || "none"}</td>
-		//     </tr>
-		//     <tr>
-		//       <th>Longitude</th>
-		//       <td>${country.longitude || "none"}</td>
-		//     </tr>
-		//     <tr>
-		//       <th>Timezones</th>
-		//       <td>Available Soon</td>
-		//     </tr>
-		//     <tr>
-		//       <th>Currencies</th>
-		//       <td>Available Soon</td>
-		//     </tr>
-		//     <tr>
-		//       <th>Languages</th>
-		//       <td>Available Soon</td>
-		//     </tr>
-		//   </table>
-		// `;
 	}
 	// displayDataInTable(data) {
 	// 	// extract desired properties from country data
@@ -264,7 +270,7 @@ class Country {
 	renderFlag(flag) {
 		const flagContainer = document.querySelector(".flag-container");
 		flagContainer.innerHTML = `
-      <img class="flag" src="${flag}" alt="Flag cannot be rendered">`;
+      <img class="flag" src="${flag}" alt="Flag of source: ${flag} cannot be rendered">`;
 		flagContainer.style.display = "block"; // display container for flag
 	}
 }
